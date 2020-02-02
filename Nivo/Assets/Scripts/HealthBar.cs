e59ec7e5339ff78bebbe645fb3c508cb78a0e8fe
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-02-03 01:29:46 +0200

    Restore The Project

---
+++ Nivo/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBar;
+    public int Starthealth = 100;
+    [HideInInspector] public float nowHealth;
+
+    private void Start()
+    {
+        nowHealth = Starthealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount =  nowHealth / Starthealth;
+        if (nowHealth <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Nivo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!"); //This command makes a debug message appear in Unity so we know if the quit button is working
+        Application.Quit(); ///Closes the game
+    }
+}
