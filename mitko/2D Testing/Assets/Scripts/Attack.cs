8d3334767526a566480814bc3d78db4e96f62852
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-02-03 15:41:47 +0200

    Map changes

---
+++ mitko/2D Testing/Assets/Scripts/Attack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public float damage = 10f;
+    private GameObject player;
+    bool attack = false;
+    private float countdown = 0f;
+
+    public void Update()
+    {
+        if (attack == true)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f)
+            {
+                TakeDamage(player);
+                countdown = 1f;
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            attack = true;
+            player = other.gameObject;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            attack = false;
+            countdown = 0f;
+        }
+    }
+
+    private void TakeDamage(GameObject obj)
+    {
+        obj.GetComponent<HealthBar>().nowHealth -= damage;
+    }
+}
+
+
