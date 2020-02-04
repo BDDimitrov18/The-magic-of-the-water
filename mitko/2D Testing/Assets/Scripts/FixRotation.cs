b321e9e4c9f36cb06ac15132c2c3eaf33ba8bc79
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-02-05 00:17:55 +0200

    SANTONE

---
+++ mitko/2D Testing/Assets/Scripts/FixRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    public Transform player;
+
+    private void Update()
+    {
+        transform.position = new Vector2(player.transform.position.x - 8f, Random.Range(player.transform.position.y-4f, player.transform.position.y + 4f));
+    }
+}
--- mitko/Assets/Scripts/FrogMovement.cs
+++ mitko/Assets/Scripts/FrogMovement.cs
@@ -4,38 +4,44 @@ using UnityEngine;
 using UnityEngine.Events;
 public class FrogMovement : MonoBehaviour
 {
-    public GameObject player;
-    public Rigidbody2D m_Rigidbody2D;
-    public Vector3 m_Velocity=Vector3.zero;
-    float xdif = 0f;
-    public Animator anim;
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
-    public GameObject frogSprite;
+    public float speed;
+    public float distance;
 
-    
-    private void FixedUpdate()
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+    public Transform wallDetection;
+
+    void Update()
     {
-        xdif = gameObject.transform.position.x - player.transform.position.x;
-        if (xdif>1 && xdif<20)
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        RaycastHit2D wallInfo = Physics2D.Raycast(wallDetection.position, Vector2.right, distance);
+        if (groundInfo.collider == false)
         {
-            frogSprite.transform.eulerAngles = new Vector3(0f, 0f, 0f);
-            Vector3 targetVelocityj = new Vector2(1 * -2,m_Rigidbody2D.velocity.y);
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocityj, ref m_Velocity, m_MovementSmoothing);
-            anim.SetBool("IsWalking", true);
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
         }
-        else if(xdif < -1 && xdif > -20)
+        if (wallInfo.collider == true && wallInfo.collider.gameObject.layer == 9)
         {
-            frogSprite.transform.eulerAngles = new Vector3(0f, 180f, 0f);
-            Vector3 targetVelocityj = new Vector2(1 * 2, m_Rigidbody2D.velocity.y);
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocityj, ref m_Velocity, m_MovementSmoothing);
-            anim.SetBool("IsWalking", true);
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
         }
-        else
-        {
-            m_Rigidbody2D.velocity = Vector3.zero;
-            anim.SetBool("IsWalking", false);
-        }
-        
     }
-
 }
